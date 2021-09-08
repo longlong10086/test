5ceed09266618772b1536a13f58601452e804097
tmp1 <tmp1@example.com>
2021-09-08 18:01:01 +0800

    first commit

---
+++ src/all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+pwdd=$(cd "$(dirname "$0")"; pwd)
+echo 
+cd $pwdd
+
+source ../etc/bashrc1
+source ../etc/bashrc2
+
+. ./mysh.sh
---
+++ src/mysh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "=============begin mysh.sh================="
+
+date >/tmp/mysh.log
+env  >>/tmp/mysh.log
+
+echo "testa=$testa ##"
+echo "testb=$testb ##"
+echo "testa1=$testa1 ##"
+echo "testb1=$testb1 ##"
+echo "testc=$testc ##"
+echo "testd=$testd ##"
+
+echo "too_c=$too_c ##"
+echo "too_d=$too_d ##"
+
+echo "=============end mysh.sh ==================="
